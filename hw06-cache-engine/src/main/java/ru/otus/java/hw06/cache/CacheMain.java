332e398ac0902824a2f4f84617252a9250647dfb
woworks <artemslastin@gmail.com>
2019-03-11 00:12:04 +0200

    hw06 - cache engine based on soft references - wip

---
+++ hw06-cache-engine/src/main/java/ru/otus/java/hw06/cache/CacheMain.java
@@ -0,0 +1,58 @@
+package ru.otus.java.hw06.cache;
+
+/**
+ * Created by tully.
+ */
+public class CacheMain {
+
+
+    public void eternalCacheExample() {
+        int size = 5;
+        CacheEngine<Integer, String> cache = new CacheEngineImpl<>(size, 0, 0, true);
+
+        for (int i = 0; i < 10; i++) {
+            cache.put(new MyElement<>(i, "String: " + i));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            MyElement<Integer, String> element = cache.get(i);
+            System.out.println("String for " + i + ": " + (element != null ? element.getValue() : "null"));
+        }
+
+        System.out.println("Cache hits: " + cache.getHitCount());
+        System.out.println("Cache misses: " + cache.getMissCount());
+
+        cache.dispose();
+    }
+
+    public void lifeCacheExample() throws InterruptedException {
+        int size = 5;
+
+        CacheEngine<Integer, String> cache = new CacheEngineImpl<>(size, 1000, 0, false);
+
+        for (int i = 0; i < size; i++) {
+            cache.put(new MyElement<>(i, "String: " + i));
+        }
+
+        for (int i = 0; i < size; i++) {
+            MyElement<Integer, String> element = cache.get(i);
+            System.out.println("String for " + i + ": " + (element != null ? element.getValue() : "null"));
+        }
+
+        System.out.println("Cache hits: " + cache.getHitCount());
+        System.out.println("Cache misses: " + cache.getMissCount());
+
+        Thread.sleep(1000);
+
+        for (int i = 0; i < size; i++) {
+            MyElement<Integer, String> element = cache.get(i);
+            System.out.println("String for " + i + ": " + (element != null ? element.getValue() : "null"));
+        }
+
+        System.out.println("Cache hits: " + cache.getHitCount());
+        System.out.println("Cache misses: " + cache.getMissCount());
+
+        cache.dispose();
+    }
+
+}
---
+++ hw06-cache-engine/src/main/java/ru/otus/java/hw06/cache/SoftCacheEngineImpl.java
@@ -0,0 +1,97 @@
+package ru.otus.java.hw06.cache;
+
+import java.lang.ref.SoftReference;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Function;
+
+/**
+ * Created by woworks.
+ */
+public class SoftCacheEngineImpl<K, V> implements CacheEngine<K, V> {
+    private static final int TIME_THRESHOLD_MS = 5;
+
+    private final int maxElements;
+    private final long lifeTimeMs;
+    private final long idleTimeMs;
+    private final boolean isEternal;
+
+    private final Map<SoftReference<K>, MyElement<K, V>> elements = new LinkedHashMap<>();
+    private final Timer timer = new Timer();
+
+    private int hit = 0;
+    private int miss = 0;
+
+    public SoftCacheEngineImpl(int maxElements, long lifeTimeMs, long idleTimeMs, boolean isEternal) {
+        this.maxElements = maxElements;
+        this.lifeTimeMs = lifeTimeMs > 0 ? lifeTimeMs : 0;
+        this.idleTimeMs = idleTimeMs > 0 ? idleTimeMs : 0;
+        this.isEternal = lifeTimeMs == 0 && idleTimeMs == 0 || isEternal;
+    }
+
+    public void put(MyElement<K, V> element) {
+        if (elements.size() == maxElements) {
+
+            K firstKey = elements.keySet().iterator().next().get();
+            elements.remove(firstKey);
+        }
+
+        K key = element.getKey();
+        elements.put(new SoftReference<>(key), element);
+
+        if (!isEternal) {
+            if (lifeTimeMs != 0) {
+                TimerTask lifeTimerTask = getTimerTask(key, lifeElement -> lifeElement.getCreationTime() + lifeTimeMs);
+                timer.schedule(lifeTimerTask, lifeTimeMs);
+            }
+            if (idleTimeMs != 0) {
+                TimerTask idleTimerTask = getTimerTask(key, idleElement -> idleElement.getLastAccessTime() + idleTimeMs);
+                timer.schedule(idleTimerTask, idleTimeMs, idleTimeMs);
+            }
+        }
+    }
+
+    public MyElement<K, V> get(K key) {
+        MyElement<K, V> element = elements.get(new SoftReference<>(key));
+        if (element != null) {
+            hit++;
+            element.setAccessed();
+        } else {
+            miss++;
+        }
+        return element;
+    }
+
+    public int getHitCount() {
+        return hit;
+    }
+
+    public int getMissCount() {
+        return miss;
+    }
+
+    @Override
+    public void dispose() {
+        timer.cancel();
+    }
+
+    private TimerTask getTimerTask(final K key, Function<MyElement<K, V>, Long> timeFunction) {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                MyElement<K, V> element = elements.get(key);
+                if (element == null || isT1BeforeT2(timeFunction.apply(element), System.currentTimeMillis())) {
+                    elements.remove(key);
+                    this.cancel();
+                }
+            }
+        };
+    }
+
+
+    private boolean isT1BeforeT2(long t1, long t2) {
+        return t1 < t2 + TIME_THRESHOLD_MS;
+    }
+}
