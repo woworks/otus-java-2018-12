6a74be383a1352a3c46ffcb780c943ba21af8d5b
woworks <artemslastin@gmail.com>
2019-03-20 23:27:46 +0200

    hw07 - atm emulator (code review #1 changes)

--- hw07-atm/src/main/java/ru/otus/java/hw07/atm/AtmProcessorImpl.java
+++ hw07-atm/src/main/java/ru/otus/java/hw07/atm/AtmProcessorImpl.java
@@ -1,14 +1,15 @@
 package ru.otus.java.hw07.atm;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class AtmProcessorImpl implements AtmProcessor {
 
-    private User user;
+    private AtmStorage atmStorage;
 
-    public AtmProcessorImpl(User user) {
-        this.user = user;
+    public AtmProcessorImpl(AtmStorage atmStorage) {
+        this.atmStorage = atmStorage;
     }
 
     @Override
@@ -33,30 +34,45 @@ public class AtmProcessorImpl implements AtmProcessor {
             amount = sc.nextInt();
         } while (amount <= 0);
 
-        this.user.withdrawCash(amount);
+        Map<AtmStorage.Banknote, Integer> banknotes = this.atmStorage.withdrawCash(amount);
 
-        System.out.println("You'll get the following banknotes: ");
-        for (Map.Entry<CashAmount.Banknote, Integer> entry: CashAmount.calculateBanknotes(amount).entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue() + " banknotes");
+        System.out.println("You'll get the following Banknotes: ");
+        for (Map.Entry<AtmStorage.Banknote, Integer> entry: banknotes.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " Banknote(s)");
         }
     }
 
     @Override
     public void depositCash() {
 
-        int amount;
-        do {
-            System.out.print("Please correct input cash amount to deposit: ");
-            Scanner sc = new Scanner(System.in);
-            amount = sc.nextInt();
-        } while (amount <= 0);
+        Map<AtmStorage.Banknote, Integer> banknotesToDeposit = new HashMap<>();
 
-        this.user.depositCash(amount);
+        System.out.println("Please correct input cash amount to deposit: ");
+
+        for (int i = 0; i < AtmStorage.Banknote.values().length; i++) {
+
+            int amount;
+            do {
+                System.out.print("Put banknotes of amount " + AtmStorage.Banknote.values()[i] + ": ");
+                Scanner sc = new Scanner(System.in);
+                amount = sc.nextInt();
+            } while (amount <= 0);
+
+            banknotesToDeposit.put(AtmStorage.Banknote.values()[i], amount);
+
+        }
+
+        this.atmStorage.depositCash(banknotesToDeposit);
 
     }
 
     @Override
     public void checkBalance() {
-        System.out.println("Your balance: " + user.getBalance().getValue());
+        System.out.println("ATM balance: ");
+        for (int i = 0; i < AtmStorage.Banknote.values().length; i++) {
+            AtmStorage.Banknote banknote = AtmStorage.Banknote.values()[i];
+            System.out.println(banknote + ": " + atmStorage.getStorageBanknotes().get(banknote));
+        }
+
     }
 }
