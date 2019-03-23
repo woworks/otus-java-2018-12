20e403bc351479ace51f42d559b62f8fe2f6b87a
woworks <artemslastin@gmail.com>
2019-03-23 17:00:53 +0200

    hw07 - atm emulator (code review #2 changes)

--- hw07-atm/src/main/java/ru/otus/java/hw07/atm/AtmProcessorImpl.java
+++ hw07-atm/src/main/java/ru/otus/java/hw07/atm/AtmProcessorImpl.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class AtmProcessorImpl implements AtmProcessor {
 
-    private AtmStorage atmStorage;
+    private final AtmStorage atmStorage;
 
     public AtmProcessorImpl(AtmStorage atmStorage) {
         this.atmStorage = atmStorage;
@@ -34,10 +34,10 @@ public class AtmProcessorImpl implements AtmProcessor {
             amount = sc.nextInt();
         } while (amount <= 0);
 
-        Map<AtmStorage.Banknote, Integer> banknotes = this.atmStorage.withdrawCash(amount);
+        Map<Banknote, Integer> banknotes = this.atmStorage.withdrawCash(amount);
 
         System.out.println("You'll get the following Banknotes: ");
-        for (Map.Entry<AtmStorage.Banknote, Integer> entry: banknotes.entrySet()) {
+        for (Map.Entry<Banknote, Integer> entry: banknotes.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue() + " Banknote(s)");
         }
     }
@@ -45,20 +45,20 @@ public class AtmProcessorImpl implements AtmProcessor {
     @Override
     public void depositCash() {
 
-        Map<AtmStorage.Banknote, Integer> banknotesToDeposit = new HashMap<>();
+        Map<Banknote, Integer> banknotesToDeposit = new HashMap<>();
 
         System.out.println("Please correct input cash amount to deposit: ");
 
-        for (int i = 0; i < AtmStorage.Banknote.values().length; i++) {
+        for (int i = 0; i < Banknote.values().length; i++) {
 
             int amount;
             do {
-                System.out.print("Put banknotes of amount " + AtmStorage.Banknote.values()[i] + ": ");
+                System.out.print("Put banknotes of amount " + Banknote.values()[i] + ": ");
                 Scanner sc = new Scanner(System.in);
                 amount = sc.nextInt();
             } while (amount <= 0);
 
-            banknotesToDeposit.put(AtmStorage.Banknote.values()[i], amount);
+            banknotesToDeposit.put(Banknote.values()[i], amount);
 
         }
 
@@ -69,8 +69,8 @@ public class AtmProcessorImpl implements AtmProcessor {
     @Override
     public void checkBalance() {
         System.out.println("ATM balance: ");
-        for (int i = 0; i < AtmStorage.Banknote.values().length; i++) {
-            AtmStorage.Banknote banknote = AtmStorage.Banknote.values()[i];
+        for (int i = 0; i < Banknote.values().length; i++) {
+            Banknote banknote = Banknote.values()[i];
             System.out.println(banknote + ": " + atmStorage.getStorageBanknotes().get(banknote));
         }
 
