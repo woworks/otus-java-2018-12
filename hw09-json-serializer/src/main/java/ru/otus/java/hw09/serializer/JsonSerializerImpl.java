49eb0152df2680e10e333a5a0e972fe36100ba10
woworks <artemslastin@gmail.com>
2019-04-20 16:22:17 +0300

    hw09 - json serializer

--- hw09-json-serializer/src/main/java/ru/otus/java/hw09/serializer/JsonSerializerImpl.java
+++ hw09-json-serializer/src/main/java/ru/otus/java/hw09/serializer/JsonSerializerImpl.java
@@ -2,11 +2,9 @@ package ru.otus.java.hw09.serializer;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.junit.Test;
 import ru.otus.java.hw09.reflection.Reflector;
 
 import java.lang.reflect.*;
-import java.math.BigDecimal;
 import java.util.*;
 
 public class JsonSerializerImpl implements JsonSerializer {
@@ -16,93 +14,90 @@ public class JsonSerializerImpl implements JsonSerializer {
 
         Reflector reflector = new Reflector();
 
-        // get Object other than by constructor!!!
+        // Class should have constructor without params
         Object clazzObject = clazz.getDeclaredConstructor().newInstance();
 
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
-            if (field.getType().isArray()){
-                System.out.println("field " + field.getName() + " is array");
+            if (field.getType().isArray()) {
+                addArray(json, map, field);
             } else if (field.getType().equals(List.class)) {
-
-                System.out.println("field " + field.getName() + " is list");
-                List list = new ArrayList();
-                JSONArray jsonArray = (JSONArray)json.get(field.getName());
-
-                Class itemType = null;
-
-                Type type = field.getGenericType();
-                //System.out.println("type: " + type);
-                if (type instanceof ParameterizedType) {
-                    ParameterizedType pt = (ParameterizedType) type;
-                    //System.out.println("raw type: " + pt.getRawType());
-                    //System.out.println("owner type: " + pt.getOwnerType());
-                    //System.out.println("actual type args:");
-                    for (Type t : pt.getActualTypeArguments()) {
-                        System.out.println("    " + t);
-                        if (t instanceof Class) {
-                            @SuppressWarnings("unchecked")
-                            Class<?> clazz2 = (Class<?>) t;
-                            System.out.println("    clazz2 " + clazz2);
-                            itemType  = clazz2;
-                        }
-
-                    }
-                }
-
-                final Class itemClass = itemType;
-
-                jsonArray.forEach(item -> {
-
-                        field.setAccessible(true);
-                        //list.add(getObject((JSONObject) item, field.getType().getComponentType()));
-                        Class cl = field.getType();
-                        Class cl2 = field.getType().getComponentType();
-                    try {
-                        list.add(getObject((JSONObject) item, itemClass));
-                    } catch (NoSuchMethodException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    } catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    } catch (InstantiationException e) {
-                        e.printStackTrace();
-                    }
-
-                });
-
-                map.put(field.getName(), list);
+                addList(json, map, field);
             } else {
                 System.out.println("field " + field.getName() + " is simple");
                 map.put(field.getName(), json.get(field.getName()));
             }
         }
 
-
-
-        /*for(Object key: json.keySet()){
-            System.out.println("key = " + key);
-            Object val = json.get(key);
-            System.out.println("val = " + val);
-            if (val instanceof JSONArray) {
-
-                ((JSONArray)val).forEach(item -> {
-                    //item.getClass()
-                    //reflector.
-                    //map.put((String) key, getObjectArray());
-
-                });
-            } else {
-                map.put((String) key, val);
-
-            }
-
-        }*/
-
         reflector.populateFields(clazzObject, map);
         return clazzObject;
 
+    }
+
+    private void addList(JSONObject json, Map<String, Object> map, Field field) {
+        System.out.println("field " + field.getName() + " is list");
+        List list = new ArrayList();
+        JSONArray jsonArray = (JSONArray) json.get(field.getName());
+
+        Class itemType = null;
+
+        Type type = field.getGenericType();
+        if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType) type;
+            for (Type t : pt.getActualTypeArguments()) {
+                if (t instanceof Class) {
+                    itemType = (Class<?>) t;
+                    break;
+                }
+            }
+        }
+
+        final Class itemClass = itemType;
+
+        jsonArray.forEach(item -> {
+
+            field.setAccessible(true);
+            try {
+                list.add(getObject((JSONObject) item, itemClass));
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        map.put(field.getName(), list);
+    }
+
+    private void addArray(JSONObject json, Map<String, Object> map, Field field) {
+        System.out.println("field " + field.getName() + " is array");
+        JSONArray jsonArray = (JSONArray) json.get(field.getName());
+
+        Type itemClass = field.getType().getComponentType();
+
+        try {
+            itemClass = getClass(itemClass.getTypeName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Object array = Array.newInstance((Class<?>) itemClass, jsonArray.size());
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+
+            field.setAccessible(true);
+            try {
+                if (((Class) itemClass).isPrimitive()) {
+                    Array.set(array, i, jsonArray.get(i));
+                } else {
+                    Array.set(array, i, getObject((JSONObject) jsonArray.get(i), (Class) itemClass));
+                }
+
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
+
+        map.put(field.getName(), array);
     }
 
     @Override
@@ -137,27 +132,26 @@ public class JsonSerializerImpl implements JsonSerializer {
             } else {
                 jsonArray.add(serialize(Array.get(objects, i)));
             }
-
         }
         return jsonArray;
     }
 
     private JSONArray serializeList(Object objects) {
         JSONArray jsonArray = new JSONArray();
-        for (Object object: (List) objects) {
+        for (Object object : (List) objects) {
             jsonArray.add(serialize(object));
         }
         return jsonArray;
     }
 
-    private Class getListItemType(List list) throws NoSuchFieldException {
-
-
-        Field stringListField = Test.class.getDeclaredField("stringList");
-        ParameterizedType stringListType = (ParameterizedType) stringListField.getGenericType();
-        Class<?> stringListClass = (Class<?>) stringListType.getActualTypeArguments()[0];
-        System.out.println(stringListClass);
-
-        return String.class;
+    private Class getClass(String className) throws ClassNotFoundException {
+        if ("int".equals(className)) return int.class;
+        if ("long".equals(className)) return long.class;
+        if ("double".equals(className)) return double.class;
+        if ("float".equals(className)) return float.class;
+        if ("byte".equals(className)) return byte.class;
+        if ("char".equals(className)) return char.class;
+        if ("boolean".equals(className)) return boolean.class;
+        return Class.forName(className);
     }
 }
